7664a7291c0ff188701e6a441af2f274c8f3bc13
Loghi <loghijiaha.16@cse.mrt.ac.lk>
2020-08-07 15:30:10 +0530

    Update train.py

--- deepspeech_training/train.py
+++ deepspeech_training/train.py
@@ -896,8 +896,7 @@ def do_single_file_inference(source):
               output_features.append(logits)
     output = np.array(output_features)
     np.save('/content/DeepSpeech/data/ds_decode_tamil data_v2_padded.npy', output)
-    print(output)
-
+    
 def early_training_checks():
     # Check for proper scorer early
     if FLAGS.scorer_path:
